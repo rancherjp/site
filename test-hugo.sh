0d697d5e3a6d0d089303e67d5624e9d4b41db8b7
foxboxsnet <naokiaoyama2009@gmail.com>
2016-12-24 00:17:48 +0900

    CircleCI test

--- test-hugo.sh
+++ test-hugo.sh
@@ -8,20 +8,24 @@ commitMassage=`git log -n 1 --oneline --pretty=format:"%s"`
 
 
 
-
-
 if [[ "$commitMassage" =~ \["Site Update"\] ]]; then
+        hugo -d docs
         git add ./docs/ -A
         git commit -m "[ci skip] CircleCI automatic page build Time:[`date`]" || true
         git push origin ${GITHUB_BRANCH}
         echo "[Site Update] Merge it to www.rancher.jp is skipped..."
 else
         echo "[Site Update] Merge it to www.rancher.jp is start..."
+        sed 's/"http://www.rancher.jp/site/"/"http://www.rancher.jp/"/' config.toml
+        sed '/noindex/d' config.toml
+        cat config.toml
+        hugo -d docs
+
         cd ~/
         git clone git@github.com:rancherjp/rancherjp.github.io.git
-        mv -v ~/site ~/rancherjp.github.io
+        mv -v ~/site/docs ~/rancherjp.github.io/
         cd ~/rancherjp.github.io
-        git add .
+        git add . -A
         git commit -m "CircleCI automatic page build Time:[`date`]" || true
         git push origin ${GITHUB_BRANCH}
 fi
