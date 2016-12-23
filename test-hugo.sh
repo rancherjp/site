a42036e97fb81e00ddca25e7fcc249f8f2fdb8c2
foxboxsnet <naokiaoyama2009@gmail.com>
2016-12-24 00:33:03 +0900

    CircleCI test

--- test-hugo.sh
+++ test-hugo.sh
@@ -16,14 +16,13 @@ if [[ "$commitMassage" =~ \["Site Update"\] ]]; then
         echo "[Site Update] Merge it to www.rancher.jp is skipped..."
 else
         echo "[Site Update] Merge it to www.rancher.jp is start..."
-        sed 's/"http://www.rancher.jp/site/"/"http://www.rancher.jp/"/' config.toml
-        sed '/noindex/d' config.toml
-        cat config.toml
+        sed -i -e 's@http://www.rancher.jp/site/@http://www.rancher.jp/@g' config.toml
+        sed -i -e '/noindex/d' config.toml
         hugo -d docs
 
         cd ~/
         git clone git@github.com:rancherjp/rancherjp.github.io.git
-        mv -v ~/site/docs ~/rancherjp.github.io/
+        cp -v ~/site/docs ~/rancherjp.github.io
         cd ~/rancherjp.github.io
         git add . -A
         git commit -m "CircleCI automatic page build Time:[`date`]" || true
